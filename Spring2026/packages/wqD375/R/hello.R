e71de4b7168767a78cfb67dc0f50d29e64afa963
id8493 <id8493@bard.edu>
2026-04-20 01:31:13 -0400

    fixed issues with the data set

--- Spring2026/packages/wqD375/R/hello.R
+++ Spring2026/packages/wqD375/R/hello.R
@@ -25,7 +25,9 @@ wq_clean_names <- c(
   month = "Month",
   season = "Season",
   date = "Date",
-  week_n = "Week")
+  week_n = "Week",
+  date = "Date",
+  season_num = "Season")
 
 # function to streamline scatter plots
 wq_scatterplot <- function(df, var1, var2, plot_title = NULL) {
@@ -50,13 +52,12 @@ wq_scatterplot <- function(df, var1, var2, plot_title = NULL) {
 }
 
 # function to streamline box plots
-wq_boxplot <- function(df, var1, var2) {
+wq_boxplot <- function(df, var1, var2, plot_title = NULL) {
 
   # creating
   var1_name <- as_name(ensym(var1))
   var2_name <- as_name(ensym(var2))
 
-  label  <- englue("{var1_name} {var2_name}")
 
   # accessing the variable name from the wq_clean_names object and assigning it to the x and y labels
   x_label <- wq_clean_names[[var1_name]]
@@ -66,36 +67,30 @@ wq_boxplot <- function(df, var1, var2) {
     ggplot() +
     geom_boxplot(aes(x = {{var1}}, y = {{var2}})) +
     labs(
-      title = label,
+      title = plot_title,
       x = x_label,
       y = y_label) +
     theme_minimal()
 }
 
 # function to streamline line graphs. Ensure the way is a valid time variable
-wq_line_graph <- function(df, var1, var2, var3 = NULL, plot_title = NULL) {
+wq_line_graph <- function(df, var1, var2, plot_title = NULL) {
 
   # creating
   var1_name <- as_name(ensym(var1))
   var2_name <- as_name(ensym(var2))
-  var3_name <- as_name(ensym(var3))
-
-
-  label  <- englue("{var1_name} {var2_name}")
 
   # accessing the variable name from the wq_clean_names object and assigning it to the x and y labels
   x_label <- wq_clean_names[[var1_name]]
   y_label <- wq_clean_names[[var2_name]]
-  color_label <- wq_clean_names[[var3_name]]
 
   df |>
     ggplot() +
-    geom_line(aes(x = {{var1}}, y = {{var2}}, color = {{var3}})) +
+    geom_line(aes(x = {{var1}}, y = {{var2}})) +
     labs(
-      title = {{plot_title}},
+      title = plot_title,
       x = x_label,
-      y = y_label,
-      color = var3_name) +
+      y = y_label) +
     theme_minimal()
 }
 
